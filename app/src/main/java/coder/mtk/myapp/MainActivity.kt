abbd6e07d49d6d0d0bda7c9162da292847387f9d
Min Thaw Khant <minnthawkhantt@gmail.com>
2020-07-29 14:56:57 +0630

    upload layout exercise

---
+++ app/src/main/java/coder/mtk/myapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package coder.mtk.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.exercise_layout)
+
+
+    }
+   }
